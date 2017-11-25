be1c85411e3dbb8f5849460a276c20d664df3c2f
ladislavbili <l.bilisics@gmail.com>
2017-11-25 12:18:59 +0100

    pages

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import { CircularProgress } from 'material-ui/Progress';
+
+import {loginUser} from '../../redux/actions';
+import Navigation from '../navigation';
+
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:'admin',
+      password:'admin',
+    };
+  }
+  handleInputChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
   render() {
-    return (<div>Loading...</div>);
+    if(this.props.authenticated){
+      return (<Navigation/>);
+    }
+    return (
+      <Paper elevation={4} style={{width:250}}>
+      <Typography type="headline" component="h4">
+        Username:
+      </Typography>
+      <Typography type="body1" component="div">
+        <TextField
+          label="Username"
+          value={this.state.username}
+          onChange={this.handleInputChange("username")}
+          autoComplete="current-username"
+          margin="normal"
+        />
+      </Typography>
+      <Typography type="headline" component="h4">
+        Password:
+      </Typography>
+      <Typography type="body1" component="div">
+        <TextField
+          label="Password"
+          type="password"
+          value={this.state.password}
+          onChange={this.handleInputChange("password")}
+          autoComplete="current-password"
+          margin="normal"
+        />
+        </Typography>
+      <Typography type="body1" component="div">
+        <Button raised color="primary" onClick={()=>this.props.loginUser(this.state.username,this.state.password)}>
+          {this.props.loading?<CircularProgress style={{color:'white'}} size={25} />:"Login"}
+        </Button>
+      </Typography>
+
+      {
+        this.props.error &&
+        <Typography type="body1" component="div" style={{color:'red'}}>
+          Login has failed! Please check your username or password.
+        </Typography>
+    }
+      </Paper>
+    );
   }
 }
 
 const mapStateToProps = ({ login }) => {
-  const { authenticated,loading } = login;
-  return {authenticated, loading};
+  const { authenticated,loading,error } = login;
+  return {authenticated, loading,error};
 };
 
-export default connect(mapStateToProps, {})(Login);
+export default connect(mapStateToProps, {loginUser})(Login);
--- src/redux/actions/loginActions.js
+++ src/redux/actions/loginActions.js
@@ -1,49 +1,6 @@
-import {LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL, LOGIN_LOGOUT, START_LOADING, START_LOADING_PROJECTS, SET_PROJECTS, SET_FILTERS, SET_TOKEN } from '../types';
-import {LOGIN_URL, PROJECT_LIST,FILTER_LIST} from '../urls';
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL } from '../types';
+import { LOGIN_URL } from '../urls';
 import jwt_decode from 'jwt-decode';
-//All of these are actions, they return redux triggered functions, that have no return, just manipulate with the store
-
-/**
- * Loads all projects after user has been logged in
- * @param  {function} dispatch Accesses and modifies redux store via Actions
- * @param  {string} token    Token for the REST API
- */
-function getProjects(dispatch,token){
-   fetch(PROJECT_LIST+'?limit=999', {
-     method: 'GET',
-     headers: {
-       'Authorization': 'Bearer ' + token
-     }
-   }).then((response) =>{
-     response.json().then((data) => {
-       dispatch({type: SET_PROJECTS, payload:{projects:data.data}});
-     });
-   }
- ).catch(function (error) {
-   console.log(error);
- });
-}
-
-/**
- * Loads all filters after user has been logged in
- * @param  {function} dispatch Accesses and modifies redux store via Actions
- * @param  {string} token    Token for the REST API
- */
- function getFilters(dispatch,token){
-   fetch(FILTER_LIST+'?limit=999', {
-     method: 'GET',
-     headers: {
-       'Authorization': 'Bearer ' + token
-     }
-   }).then((response) =>{
-     response.json().then((data) => {
-       dispatch({type: SET_FILTERS, filters:data.data});
-     });
-   }
- ).catch(function (error) {
-   console.log(error);
- });
-}
 
 /**
  * Tries to log in user using his username and password
@@ -60,85 +17,21 @@ function getProjects(dispatch,token){
      }).then((JSONresponse) => {
        JSONresponse.json().then((response)=>{
          if(JSONresponse.ok){
-           storeTokenToAsyncStorage(response.token);
-           getFilters(dispatch,response.token);
-           getProjects(dispatch,response.token);
            let user=jwt_decode(response.token);
+           console.log(user);
            dispatch({
              type: LOGIN_SUCCESS,
              payload: {user,token:response.token}
            });
-           Actions.taskList();
          }
          else{
-           loginUserFail(dispatch);
+           dispatch({ type: LOGIN_FAIL });
          }
        });
      })
      .catch(function (error) {
        console.log(error);
-       loginUserFail(dispatch);
+       dispatch({ type: LOGIN_FAIL });
      });
    };
  };
-
-/**
- * Log's in user via existing token
- * @param  {string} token  Token for the REST API
- */
- export const loginUserWithToken = (token) => {
-   return (dispatch) => {
-     let user=jwt_decode(token);
-     user['ACL']=user.userRoleAcl;
-     getFilters(dispatch,token);
-     getProjects(dispatch,token);
-     dispatch({
-       type: LOGIN_SUCCESS,
-       payload: {user,token}
-     });
-     Actions.taskList();
-   };
- };
-
-/**
- * Log's out user and returns user to the login page
- */
- export const logoutUser = () => {
-   return (dispatch) => {
-     dispatch({ type: LOGIN_LOGOUT });
-     removeTokenFromAsyncStorage();
-     Actions.login();
-   }
- };
-
-/**
- * Set's the current token
- * @param {string} token Token for the REST API
- */
- export const setToken = (token) => {
-   return (dispatch) => {
-     dispatch({ type: SET_TOKEN, token });
-   }
- };
-
-/**
- * User login failed, stops login indicator and displays an error
- * @param  {function} dispatch Accesses and modifies redux store via Actions
- */
-const loginUserFail = (dispatch) => {
-  dispatch({ type: LOGIN_FAIL });
-};
-
-/**
- * Store the token into device's async storage
- * @param  {string} token Token to be saved
- */
-export const storeTokenToAsyncStorage = (token) =>
-  AsyncStorage.setItem('lansystem-v1-token',token);
-
-  /**
-   * Store the token into device's async storage
-   * @param  {string} token Token to be saved
-   */
-export const removeTokenFromAsyncStorage = () =>
-  AsyncStorage.multiRemove(['lansystem-v1-token']);
